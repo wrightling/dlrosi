936d982214362c3d1a385caf5ab6053cdb12d329
Warren Wright <warren@thewrightangle.com>
2016-12-05 13:35:23 -0800

    format output during clone process

--- lib/dlrosi/clone.ex
+++ lib/dlrosi/clone.ex
@@ -2,12 +2,14 @@ defmodule Dlrosi.Clone do
   def clone_repos(repos) do
     repos
     |> Enum.map(&(Task.async(Dlrosi.Clone, :clone_repo, [&1])))
-    |> Enum.each(&(Task.await(&1, seconds_to_ms(60))))
+    |> Enum.map(&(Task.await(&1, seconds_to_ms(60))))
   end
 
   def clone_repo(repo) do
+    log_clone_start(repo)
     File.cd!(path_to(repo), fn -> clone(repo) end)
-    {:ok, "whatever"}
+    log_clone_finish(repo)
+    :ok
   end
 
   defp base_dir, do: Dlrosi.Directories.base_dir
@@ -24,5 +26,23 @@ defmodule Dlrosi.Clone do
 
   defp clone(repo) do
     Git.clone!([git_url(repo_name(repo)), "#{path_to(repo)}/#{repo_name(repo)}"])
+  end
+
+  defp format(output) do
+    IO.ANSI.format(output)
+    |> IO.iodata_to_binary
+  end
+
+  def log_clone_start(repo) do
+    IO.puts format([:cyan, "[-] ",
+                   :default_color, "Cloning ",
+                   :magenta, "#{repo_name(repo)} ",
+                   :default_color, "into #{path_to(repo)}"])
+  end
+
+  def log_clone_finish(repo) do
+    IO.puts format([:cyan, "[+] ",
+                    :magenta, "#{repo_name(repo)}",
+                    :default_color, "...Finished!"])
   end
 end
--- test/clone_test.exs
+++ test/clone_test.exs
@@ -10,8 +10,9 @@ defmodule CloneTest do
                  {Git, [], [clone!: fn(_path) -> :ok end]},
                  {Dlrosi.Directories, [], [base_dir: fn -> "." end]}
                ]) do
-      {result, output} = clone_repo({".", "fake_repo_name"})
-      assert result == :ok
+      # assert clone_repo({".", "fake_repo_name"}) == :ok
+      assert ExUnit.CaptureIO.capture_io(fn -> clone_repo({".", "fake_repo_name"}) end)
+        =~ ~r/.*Cloning.*fake_repo_name.*/
       assert called Git.clone!(["git@bitbucket.org:stelladot/fake_repo_name.git",
                                 "././fake_repo_name"])
     end
